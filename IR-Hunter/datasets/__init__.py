50b31017c701fd36221abdd0e4eba1567c77d476
ZD-zhu <1908144107@qq.com>
2025-08-02 21:22:16 +0800

    Delete IR-Hunter/datasets/__init__.py

--- IR-Hunter/datasets/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2022 CircuitNet. All rights reserved.
-
-from .congestion_dataset import CongestionDataset
-from .drc_dataset import DRCDataset
-from .irdrop_dataset import IRDropDataset
-from .train_dataset import TrainDataset
-
-__all__ = ['CongestionDataset', 'DRCDataset', 'IRDropDataset','TrainDataset']
